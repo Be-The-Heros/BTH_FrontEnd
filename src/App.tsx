3786de49dfab85f51b9ea4b051f9751523828bdf
Huy Trương Thành <ripker1805@gmail.com>
2022-01-03 11:22:43 +0700

    set up recoil and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="home-screen">
+      <header className="home-screen__header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
