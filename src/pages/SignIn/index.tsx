5c0cce0f7b583b2f4605909c58a6c0136b1a1ca7
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2022-03-16 10:51:14 +0700

    fix conflict

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,19 @@
-import icon_fb from 'assets/images/icon_fb.svg';
-import icon_gg from 'assets/images/icon_gg.svg';
-import Loading from 'components/Loading';
-import { User } from 'firebase/auth';
-import { setLocalStorage } from 'helpers/setTitleDocument';
-import { useSignIn } from 'hooks/auth/signIn';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
-import { useNavigate } from 'react-router';
-import { useSetRecoilState } from 'recoil';
-import { userState } from 'recoil/users/state';
-import { signInWithGoogleAuth } from 'services/firebase';
-import StyleSignIn from './style';
+import icon_fb from "assets/images/icon_fb.svg";
+import icon_gg from "assets/images/icon_gg.svg";
+import Loading from "components/Loading";
+import { User } from "firebase/auth";
+import { setLocalStorage } from "helpers/setTitleDocument";
+import { useSignIn } from "hooks/auth/signIn";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useNavigate } from "react-router";
+import { useSetRecoilState } from "recoil";
+import { userState } from "recoil/users/state";
+import { signInWithGoogleAuth } from "services/firebase";
+import StyleSignIn from "./style";
 
-<<<<<<< HEAD
-import {} from "components/Button";
-import Style from "./style";
-import {
-  useLoginByEmail,
-  useRegisterUserByGoogle,
-  UseRegisterUserByGoogle,
-} from "hooks/user";
-import { SubmitHandler, useForm } from "react-hook-form";
-
-type InputsLogin = {
-=======
 interface InputGroup {
->>>>>>> 2a9936c32b39764f6e02f8e3b0f4159ab10c1115
   email: string;
   password: string;
 }
@@ -46,14 +33,14 @@ const SignInPage = () => {
       email: payload.email,
       uid_gg: payload.uid,
       photo_url: payload.photoURL,
-      type: 'google',
+      type: "google",
     });
   };
   const handleSignInManual = (data: InputGroup) => {
     mutation.mutate({
       email: data.email,
       password: data.password,
-      type: 'manual',
+      type: "manual",
     });
   };
   React.useEffect(() => {
@@ -62,53 +49,53 @@ const SignInPage = () => {
         ...mutation.data.data,
         isLoggedIn: true,
       });
-      setLocalStorage('user', JSON.stringify(mutation.data.data));
-      navigate('/home');
+      setLocalStorage("user", JSON.stringify(mutation.data.data));
+      navigate("/home");
     }
   }, [mutation.isSuccess]);
   return (
     <StyleSignIn>
       <form
-        className='form-sign-in'
+        className="form-sign-in"
         onSubmit={handleSubmit(handleSignInManual)}
       >
-        <div className='form-sign-in__header d-flex flex-wrap'>
-          <div className='col-6'>
-            <div className='form-sign-in__header--welcome'>
+        <div className="form-sign-in__header d-flex flex-wrap">
+          <div className="col-6">
+            <div className="form-sign-in__header--welcome">
               Welcome to <span>Be The Heroes</span>
             </div>
-            <div className='form-sign-in__header--type'>Sign in</div>
+            <div className="form-sign-in__header--type">Sign in</div>
           </div>
-          <div className='col-6'>
-            <div className='form-sign-in__header--sign-up text-right'>
+          <div className="col-6">
+            <div className="form-sign-in__header--sign-up text-right">
               Don't have an account?
             </div>
             <div
-              className='form-sign-in__header--sign-up-link text-right'
-              onClick={() => navigate('/auth/sign-up')}
+              className="form-sign-in__header--sign-up-link text-right"
+              onClick={() => navigate("/auth/sign-up")}
             >
               Sign up
             </div>
           </div>
         </div>
-        {mutation.isLoading && <Loading cover='content' />}
+        {mutation.isLoading && <Loading cover="content" />}
         {!mutation.isLoading && (
           <React.Fragment>
-            <div className='form-sign-in__content col-12'>
-              <div className='form-sign-in__content--form-input'>
-                <label className='w-100'>Enter username or email address</label>
+            <div className="form-sign-in__content col-12">
+              <div className="form-sign-in__content--form-input">
+                <label className="w-100">Enter username or email address</label>
                 <input
-                  type='email'
-                  placeholder='Username or email address'
-                  {...register('email', { required: true })}
+                  type="email"
+                  placeholder="Username or email address"
+                  {...register("email", { required: true })}
                 />
               </div>
-              <div className='form-sign-in__content--form-input'>
-                <label className='w-100'>Enter your password</label>
+              <div className="form-sign-in__content--form-input">
+                <label className="w-100">Enter your password</label>
                 <input
-                  type={isShowPassword ? 'password' : 'text'}
-                  placeholder='Password'
-                  {...register('password', { required: true })}
+                  type={isShowPassword ? "password" : "text"}
+                  placeholder="Password"
+                  {...register("password", { required: true })}
                 />
                 {isShowPassword ? (
                   <AiOutlineEyeInvisible onClick={() => toggleShowPassword()} />
@@ -118,41 +105,41 @@ const SignInPage = () => {
               </div>
             </div>
 
-            <div className='form-sign-in__footer col-12'>
-              <div className='d-flex justify-content-end'>
+            <div className="form-sign-in__footer col-12">
+              <div className="d-flex justify-content-end">
                 <span
-                  className=' form-sign-in__footer-fg'
-                  onClick={() => navigate('/auth/forgot-password')}
+                  className=" form-sign-in__footer-fg"
+                  onClick={() => navigate("/auth/forgot-password")}
                 >
                   Forgot Password?
                 </span>
               </div>
 
-              <button className='btn btn--sign-in w-100'>Sign in</button>
+              <button className="btn btn--sign-in w-100">Sign in</button>
             </div>
           </React.Fragment>
         )}
       </form>
-      <div className='plugin w-100 d-flex flex-wrap justify-content-center'>
+      <div className="plugin w-100 d-flex flex-wrap justify-content-center">
         <div
-          className='plugin-google'
+          className="plugin-google"
           style={{
-            marginLeft: '1rem',
+            marginLeft: "1rem",
           }}
           onClick={() =>
             signInWithGoogleAuth().then((res) => handleLoginGoogle(res.user))
           }
         >
-          Sign In with Google{' '}
-          <img src={icon_gg} alt={'icon-fb'} className='plugin-icon' />
+          Sign In with Google{" "}
+          <img src={icon_gg} alt={"icon-fb"} className="plugin-icon" />
         </div>
         <div
-          className='plugin-facebook'
+          className="plugin-facebook"
           style={{
-            marginLeft: '1rem',
+            marginLeft: "1rem",
           }}
         >
-          <img src={icon_fb} alt={'icon-fb'} className='plugin-icon' />
+          <img src={icon_fb} alt={"icon-fb"} className="plugin-icon" />
         </div>
       </div>
     </StyleSignIn>
