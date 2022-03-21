73c255acdfc77abcc24c0d6c439a89ba3cecdbf1
john1805 <ripker1805@gmail.com>
2022-03-21 16:35:19 +0700

    fake token

--- src/apis/urls.ts
+++ src/apis/urls.ts
@@ -1,16 +1,16 @@
 export const devUrl = {
-  kyc: "http://localhost:4000/api/kyc",
-  user: "http://localhost:5000/api/user",
-  auth: "http://localhost:9002/api/v1/auth",
-  otp: "http://localhost:5000/api/otp",
-  post: "http://localhost:5000/api/v1/post",
-  profile: "http://localhost:5000/api/v1/profile",
+  kyc: 'http://localhost:4000/api/kyc',
+  user: 'http://localhost:5000/api/user',
+  auth: 'http://localhost:5000/api/auth',
+  otp: 'http://localhost:5000/api/otp',
+  post: 'http://localhost:5000/api/post',
+  profile: 'http://localhost:5000/api/v1/profile',
 };
 export const productionUrl = {
-  kyc: "https://kyc-api.herokuapp.com/api/kyc",
-  user: "https://betheheros.tk/api/v2/nckh/user",
-  auth: "http://localhost:5000/api/auth",
-  otp: "http://localhost:5000/api/otp",
-  post: "http://localhost:5000/api/post",
-  profile: "http://localhost:9002/api/v1/profile",
+  kyc: 'https://kyc-api.herokuapp.com/api/kyc',
+  user: 'https://betheheros.tk/api/v2/nckh/user',
+  auth: 'http://localhost:5000/api/auth',
+  otp: 'http://localhost:5000/api/otp',
+  post: 'http://localhost:5000/api/post',
+  profile: 'http://localhost:9002/api/v1/profile',
 };
