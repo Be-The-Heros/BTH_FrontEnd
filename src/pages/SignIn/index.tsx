fd458121445fce695d054079fe806a88e12aefa7
john1805 <ripker1805@gmail.com>
2022-03-21 13:36:14 +0700

    Fix conflict

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import icon_fb from "assets/images/icon_fb.svg";
-import icon_gg from "assets/images/icon_gg.svg";
-import Loading from "components/Loading";
-import { User } from "firebase/auth";
-import { setLocalStorage } from "helpers/setTitleDocument";
-import { useSignIn } from "hooks/auth/signIn";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import { useNavigate } from "react-router";
-import { useSetRecoilState } from "recoil";
-import { userState } from "recoil/users/state";
-import { signInWithGoogleAuth } from "services/firebase";
-import StyleSignIn from "./style";
-=======
 import icon_fb from 'assets/images/icon_fb.svg';
 import icon_gg from 'assets/images/icon_gg.svg';
 import Loading from 'components/Loading';
@@ -31,7 +15,6 @@ import {
   signInWithGoogleAuth,
 } from 'services/firebase';
 import StyleSignIn from './style';
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
 
 interface InputGroup {
   email: string;
@@ -55,19 +38,15 @@ const SignInPage = () => {
       email: payload.email,
       uid_gg: payload.uid,
       photo_url: payload.photoURL,
-<<<<<<< HEAD
-      type: "google",
-=======
       type: 'google',
       accessToken,
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
     });
   };
   const handleSignInManual = (data: InputGroup) => {
     mutation.mutate({
       email: data.email,
       password: data.password,
-      type: "manual",
+      type: 'manual',
     });
   };
   React.useEffect(() => {
@@ -76,59 +55,54 @@ const SignInPage = () => {
         ...mutation.data.data,
         isLoggedIn: true,
       });
-<<<<<<< HEAD
-      setLocalStorage("user", JSON.stringify(mutation.data.data));
-      navigate("/home");
-=======
       setLocalStorage('token', mutation.data.data.token);
       navigate('/home');
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
     }
   }, [mutation.isSuccess]);
 
   return (
     <StyleSignIn>
       <form
-        className="form-sign-in"
+        className='form-sign-in'
         onSubmit={handleSubmit(handleSignInManual)}
       >
-        <div className="form-sign-in__header d-flex flex-wrap">
-          <div className="col-6">
-            <div className="form-sign-in__header--welcome">
+        <div className='form-sign-in__header d-flex flex-wrap'>
+          <div className='col-6'>
+            <div className='form-sign-in__header--welcome'>
               Welcome to <span>Be The Heroes</span>
             </div>
-            <div className="form-sign-in__header--type">Sign in</div>
+            <div className='form-sign-in__header--type'>Sign in</div>
           </div>
-          <div className="col-6">
-            <div className="form-sign-in__header--sign-up text-right">
+          <div className='col-6'>
+            <div className='form-sign-in__header--sign-up text-right'>
               Don't have an account?
             </div>
             <div
-              className="form-sign-in__header--sign-up-link text-right"
-              onClick={() => navigate("/auth/sign-up")}
+              className='form-sign-in__header--sign-up-link text-right'
+              onClick={() => navigate('/auth/sign-up')}
             >
               Sign up
             </div>
           </div>
         </div>
-        {mutation.isLoading && <Loading cover="content" />}
+        {mutation.isLoading && <Loading cover='content' />}
         {!mutation.isLoading && (
           <React.Fragment>
-            <div className="form-sign-in__content col-12">
-              <div className="form-sign-in__content--form-input">
-                <label className="w-100">Enter username or email address</label>
+            <div className='form-sign-in__content col-12'>
+              <div className='form-sign-in__content--form-input'>
+                <label className='w-100'>Enter username or email address</label>
                 <input
-                  type="email"
-                  placeholder="Username or email address"
-                  {...register("email", { required: true })}
+                  type='email'
+                  placeholder='Username or email address'
+                  {...register('email', { required: true })}
                 />
               </div>
-              <div className="form-sign-in__content--form-input">
-                <label className="w-100">Enter your password</label>
+              <div className='form-sign-in__content--form-input'>
+                <label className='w-100'>Enter your password</label>
                 <input
-                  type={isShowPassword ? "password" : "text"}
-                  placeholder="Password"
-                  {...register("password", { required: true })}
+                  type={isShowPassword ? 'password' : 'text'}
+                  placeholder='Password'
+                  {...register('password', { required: true })}
                 />
                 {isShowPassword ? (
                   <AiOutlineEyeInvisible onClick={() => toggleShowPassword()} />
@@ -138,26 +112,26 @@ const SignInPage = () => {
               </div>
             </div>
 
-            <div className="form-sign-in__footer col-12">
-              <div className="d-flex justify-content-end">
+            <div className='form-sign-in__footer col-12'>
+              <div className='d-flex justify-content-end'>
                 <span
-                  className=" form-sign-in__footer-fg"
-                  onClick={() => navigate("/auth/forgot-password")}
+                  className=' form-sign-in__footer-fg'
+                  onClick={() => navigate('/auth/forgot-password')}
                 >
                   Forgot Password?
                 </span>
               </div>
 
-              <button className="btn btn--sign-in w-100">Sign in</button>
+              <button className='btn btn--sign-in w-100'>Sign in</button>
             </div>
           </React.Fragment>
         )}
       </form>
-      <div className="plugin w-100 d-flex flex-wrap justify-content-center">
+      <div className='plugin w-100 d-flex flex-wrap justify-content-center'>
         <div
-          className="plugin-google"
+          className='plugin-google'
           style={{
-            marginLeft: "1rem",
+            marginLeft: '1rem',
           }}
           onClick={() =>
             signInWithGoogleAuth().then(async (res) => {
@@ -166,13 +140,13 @@ const SignInPage = () => {
             })
           }
         >
-          Sign In with Google{" "}
-          <img src={icon_gg} alt={"icon-fb"} className="plugin-icon" />
+          Sign In with Google{' '}
+          <img src={icon_gg} alt={'icon-fb'} className='plugin-icon' />
         </div>
         <div
-          className="plugin-facebook"
+          className='plugin-facebook'
           style={{
-            marginLeft: "1rem",
+            marginLeft: '1rem',
           }}
           onClick={() =>
             signInWithFacebookAuth().then((res) => {
@@ -180,7 +154,7 @@ const SignInPage = () => {
             })
           }
         >
-          <img src={icon_fb} alt={"icon-fb"} className="plugin-icon" />
+          <img src={icon_fb} alt={'icon-fb'} className='plugin-icon' />
         </div>
       </div>
     </StyleSignIn>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import Loading from "components/Loading";
-import React, { Suspense } from "react";
-=======
 import React from 'react';
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
 import {
   BrowserRouter as Router,
   Navigate,
   Route,
   Routes,
-<<<<<<< HEAD
-} from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { userState } from "recoil/users/state";
-import LayoutMain from "templates/Layout";
-import LayoutAuth from "templates/LayoutAuth";
-import ForgotPasswordPage from "./ForgotPasswordPage";
-import ProfilePage from "./ProfilePage";
-import SignInPage from "./SignIn";
-import SignUpPage from "./SignUp";
-=======
 } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { userState } from 'recoil/users/state';
@@ -28,54 +12,14 @@ import LayoutMain from 'templates/Layout';
 import LayoutAuth from 'templates/LayoutAuth';
 import ForgotPasswordPage from './ForgotPasswordPage';
 import Profile from './Profile';
+import ProfilePage from './ProfilePage';
 import SignInPage from './SignIn';
 import SignUpPage from './SignUp';
 import { VerifyEmailPage } from './VerifyEmail';
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
 
 // import CreatePostPage from './CreatePost';
 // import Homepage from './Home';
 
-<<<<<<< HEAD
-const Homepage = React.lazy(() => import("./Homepage"));
-const CreatePostPage = React.lazy(() => import("./CreatePost"));
-const ProfileSettingsPage = React.lazy(() => import("./ProfileSettings"));
-interface CustomRouteProps {
-  element?: React.LazyExoticComponent<() => JSX.Element> | JSX.Element;
-  children?: React.LazyExoticComponent<() => JSX.Element> | JSX.Element;
-}
-const PrivateRoute: React.FC<CustomRouteProps> = (props) => {
-  const { children, element } = props;
-  const [user] = useRecoilState(userState);
-  const { isLoggedIn } = user;
-  return isLoggedIn ? (
-    <Suspense fallback={<Loading cover="content" />}>
-      {element || children}
-    </Suspense>
-  ) : (
-    <Navigate to="/auth/sign-in" />
-  );
-};
-const PublicRoute: React.FC<CustomRouteProps> = (
-  props
-): React.ReactElement | null => {
-  const { children, element } = props;
-  return (
-    <Suspense fallback={<Loading cover="content" />}>
-      {element || children}
-    </Suspense>
-  );
-};
-export const AppViews = () => {
-  const user = useRecoilValue(userState);
-
-  React.useEffect(() => {
-    if (user.isLoggedIn && user.level > 1) {
-      window.location.href = "/";
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-=======
 const Homepage = React.lazy(() => import('./Homepage'));
 const CreatePostPage = React.lazy(() => import('./CreatePost'));
 const ProfileSettingsPage = React.lazy(() => import('./ProfileSettings'));
@@ -89,19 +33,23 @@ export const AppViews = () => {
   //   }
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, []);
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LayoutMain />}>
-          <Route path="/" element={<PublicRoute element={<ProfilePage />} />} />
+        <Route path='/' element={<LayoutMain />}>
+          <Route path='/' element={<PublicRoute element={<Homepage />} />} />
           <Route
-            path="/create-post"
+            path='/profile'
+            element={<PublicRoute element={<ProfilePage />} />}
+          />
+
+          <Route
+            path='/create-post'
             element={<PrivateRoute element={<CreatePostPage />} />}
           />
           <Route
-            path="/profile/settings"
+            path='/profile/settings'
             element={<PrivateRoute element={<ProfileSettingsPage />} />}
           />
           <Route
@@ -109,12 +57,6 @@ export const AppViews = () => {
             element={<PrivateRoute element={<VerifyEmailPage />} />}
           />
         </Route>
-<<<<<<< HEAD
-        <Route path="/auth" element={<LayoutAuth />}>
-          <Route path="sign-in" element={<SignInPage />} />
-          <Route path="forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="sign-up" element={<SignUpPage />} />
-=======
         <Route
           path={`/profile`}
           element={<PublicRoute element={<Profile />} />}
@@ -123,9 +65,8 @@ export const AppViews = () => {
           <Route path='sign-in' element={<SignInPage />} />
           <Route path='forgot-password' element={<ForgotPasswordPage />} />
           <Route path='sign-up' element={<SignUpPage />} />
->>>>>>> 6e4a08676c3e918669ad1915ea5149bd84e1a5c6
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </Router>
   );
