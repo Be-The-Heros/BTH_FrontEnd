4be1c7644ce95a1a3a8c399f989ed9a16a591344
john1805 <ripker1805@gmail.com>
2022-03-27 02:50:58 +0700

    change link api

--- src/apis/urls.ts
+++ src/apis/urls.ts
@@ -12,7 +12,7 @@ export const productionUrl = {
   user: 'https://betheheros.tk/api/user',
   auth: 'https://betheheros.tk/api/auth',
   otp: 'https://betheheros.tk/api/otp',
-  post: 'https://betheheros.tk/api/auth',
+  post: 'https://betheheros.tk/api/post',
   profile: 'https://betheheros.tk/api/profile',
   image: 'https://betheheros.tk/api/image',
 };
