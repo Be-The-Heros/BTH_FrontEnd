8466cc01fd527669c6181aebcfec83d33ece2cd1
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2022-04-27 20:13:10 +0700

    update

--- src/helpers/base64ToFile.ts
+++ src/helpers/base64ToFile.ts
@@ -2,11 +2,7 @@ export const base64ToFile = (base64: string) => {
   return fetch(base64)
     .then((res) => res.blob())
     .then((blob) => {
-<<<<<<< HEAD
-      const file = new File([blob], "File name", { type: "jpeg" });
-=======
-      const file = new File([blob], 'user_photo.jpg', { type: 'image/jpeg' });
->>>>>>> d547993c6ba62e9e3e1cadbcec039ee558fbcc54
+      const file = new File([blob], "user_photo.jpg", { type: "image/jpeg" });
       return file;
     });
 };
